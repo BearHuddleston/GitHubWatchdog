6123c3151fb8ff4335ddf6e2f4931a2405da8437
BearHuddleston <bear.huddleston@gmail.com>
2025-02-09 17:57:15 -0600

    remove fileutil and repo packages; migrate to database for processed users and repositories

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,36 +6,24 @@ import (
 	"os"
 )
 
-// Config holds the application settings.
 type Config struct {
-	RecordFile               string `json:"record_file"`
-	SuspiciousUserRecordFile string `json:"suspicious_user_record_file"`
-	MalRepoFile              string `json:"mal_repo_file"`
-	MalStargazerFile         string `json:"mal_stargazer_file"`
-	MaxPages                 int    `json:"max_pages"`
-	PerPage                  int    `json:"per_page"`
-	GitHubQuery              string `json:"github_query"`
-	Token                    string `json:"token"`
-	NumWorkers               int    `json:"num_workers"`
+	MaxPages      int    `json:"max_pages"`
+	PerPage       int    `json:"per_page"`
+	GitHubQuery   string `json:"github_query"`
+	Token         string `json:"token"`
+	MaxConcurrent int    `json:"max_concurrent"`
 }
 
-// New returns a new configuration by reading defaults from a config file
-// (config.json) and environment variables (which override file values).
 func New() (*Config, error) {
 	// Set default values
 	conf := Config{
-		RecordFile:               "processed_repos.txt",
-		SuspiciousUserRecordFile: "suspicious_users.txt",
-		MalRepoFile:              "malicious_repos.txt",
-		MalStargazerFile:         "malicious_stargazers.txt",
-		MaxPages:                 10,
-		PerPage:                  100,                            // maximum per page
-		GitHubQuery:              "created:>2025-01-31 stars:>5", // base query
-		Token:                    "",
-		NumWorkers:               5,
+		MaxPages:      10,
+		PerPage:       100,                            // maximum per page
+		GitHubQuery:   "created:>2025-01-31 stars:>5", // base query
+		Token:         "",
+		MaxConcurrent: 10,
 	}
 
-	// Try to read the config file if it exists.
 	configFilePath := "config.json"
 	if _, err := os.Stat(configFilePath); err == nil {
 		data, err := os.ReadFile(configFilePath)
